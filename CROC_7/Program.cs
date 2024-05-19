08371a486513a48e4d05b40d567ae9acf0253759
Cheburck <145753275+Cheburck@users.noreply.github.com>
2024-05-19 22:07:24 +0300

    first Version

---
+++ CROC_7/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace CROC_7;
+internal class Program
+{
+    public static void Main()
+    {
+        DoTask();
+        Console.WriteLine(
+            "Файл с результатом находится в папке проекта :\n" +
+            $"{FileWorker.projectPath}\n\n" +
+            $"Для выхода нажмите любую клавишу");
+        Console.ReadKey();
+    }
+
+    public static void DoTask()
+    {
+        var input = FileWorker.ReadFile("commits.txt");
+        var rating = CommitsRating.ParseFile(input);
+        var top = CommitsRating.GetTop(rating);
+        FileWorker.SaveFile("result.txt", top);
+    }
+}
