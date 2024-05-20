efacd3cb9fa1215d4f6af62c20488425bf16fcb6
Aleksey Samsonov <71924957+Samsonov-aleksey@users.noreply.github.com>
2024-05-20 21:30:42 +0300

    Update SamsonovKROKtask7.py

--- SamsonovKROKtask7.py
+++ SamsonovKROKtask7.py
@@ -13,7 +13,7 @@ def date_time_is_valid(date_time_str : str, date_time_format : str = '%Y-%m-%dT%
 #Добавим паттерны для проверки на соответсвие требованиям указанным в условии
 pattern_for_employee = re.compile(r'^[a-zA-Z0-9_]+$')
 pattern_for_hach = re.compile(r'^[a-z0-9]{7}$')
-pattern_for_date_time = re.compile(r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$')
+
 
 #Подсчет самых активных сотрудников, путем открытия файла, генератора списков и использования метода collections -> Counter().most_common()
 with open('commits.txt','r', encoding = 'utf-8') as commits_file:
@@ -24,7 +24,6 @@ with open('commits.txt','r', encoding = 'utf-8') as commits_file:
          and pattern_for_employee.match(employee) 
          and len(hach) == 7 
          and pattern_for_hach.match(hach) 
-         and pattern_for_date_time.match(date_time) 
          and date_time_is_valid(date_time)]
          ).most_common(3)
 
