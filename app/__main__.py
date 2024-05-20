5a077039dcaa68e497fe16085ac8a9f0a1484464
IvarysYT <skalin_i_i@mail.ru>
2024-05-20 19:23:07 +0300

    bugfix with file not existing

--- app/__main__.py
+++ app/__main__.py
@@ -11,9 +11,13 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+logger = logging.getLogger("main")
 
 if __name__ == "__main__":
     path = search_file("commits.txt")
+    if not path:
+        logger.error("File commits.txt not found")
+        exit(1)
     file_lines = read_file(path)
     leaders = get_leaders(file_lines)
     write_file("result.txt", leaders)
